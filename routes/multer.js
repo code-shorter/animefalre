502c66310c4fefbad429d0f3b2a18473d1ebeb7e
animefalre <166405680+animefalre@users.noreply.github.com>
2024-04-28 10:20:59 +0530

    Update multer.js

--- routes/multer.js
+++ routes/multer.js
@@ -1,33 +1,79 @@
+const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+
+// Configure Cloudinary
+cloudinary.config(process.env.CLOUDINARY_URL);
+console.log("Cloudinary connected successfully");
 
 // Define storage for images
 const imageStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        // Different File uploading paths for different cases
-        // Check file type and set destination folder accordingly
-        if (file.fieldname === 'posterImg') {
-            cb(null, './public/images/posters'); // Destination folder for posters
-        } else if (file.fieldname === 'thumbnail') {
-            cb(null, './public/images/thumbnails'); // Destination folder for thumbnails
-        } else if (file.fieldname === 'profileImg') {
-            cb(null, './public/images/profile-img'); // Destination folder for profile Img
-        } else if (file.fieldname === 'bannerImg') {
-            cb(null, './public/images/banner'); // Destination folder for banner Img
-        } else if (file.fieldname === 'seasonImg') {
-            cb(null, './public/images/season'); // Destination folder for season Img
-        } else {
-            cb(new Error('Invalid field name'));
-        }
-    },
-    filename: function (req, file, cb) {
-        const uniqueFilename = uuidv4(); // Generate a unique filename using UUID
-        cb(null, uniqueFilename + path.extname(file.originalname)); // Use the filename for the uploaded file
+  destination: function (req, file, cb) {
+    // Check file type and set destination folder accordingly
+    if (file.fieldname === "posterImg") {
+      cb(null, "./public/images/posters"); // Destination folder for posters
+    } else if (file.fieldname === "thumbnail") {
+      cb(null, "./public/images/thumbnails"); // Destination folder for thumbnails
+    } else if (file.fieldname === "profileImg") {
+      cb(null, "./public/images/profile-img"); // Destination folder for profile Img
+    } else if (file.fieldname === "bannerImg") {
+      cb(null, "./public/images/banner"); // Destination folder for banner Img
+    } else if (file.fieldname === "seasonImg") {
+      cb(null, "./public/images/season"); // Destination folder for season Img
+    } else {
+      cb(new Error("Invalid field name"));
     }
+  },
+  filename: function (req, file, cb) {
+    const uniqueFilename = uuidv4(); // Generate a unique filename using UUID
+    cb(null, uniqueFilename + path.extname(file.originalname)); // Use the filename for the uploaded file
+  },
 });
 
 const upload = multer({ storage: imageStorage });
 
-module.exports = upload;
+// NEW //
 
+async function handleImageUpload(req, res, next) {
+  try {
+    let folderName;
+    switch (req.file.fieldname) {
+      case "posterImg":
+        folderName = "posters";
+        break;
+      case "thumbnail":
+        folderName = "thumbnails";
+        break;
+      case "profileImg":
+        folderName = "profile-img";
+        break;
+      case "bannerImg":
+        folderName = "banner";
+        break;
+      case "seasonImg":
+        folderName = "season";
+        break;
+      default:
+        return res.status(400).json({ error: "Invalid field name" });
+    }
+
+    // Upload image to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: folderName, // Specify the folder in Cloudinary
+      public_id: req.file.filename,
+    });
+    
+    // Set the image URL in the Anime schema based on the fieldname
+    req.imageURL = result.secure_url;
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+}
+
+module.exports = {
+  upload,
+  handleImageUpload,
+};
